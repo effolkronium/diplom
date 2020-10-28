637d6de9c0826166b0ac8b080cd186cff6a296d1
effolkronium <effolkronium@gmail.com>
2020-10-28 20:24:40 +0200

    Add multi-threaded compiling for linux

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ echo "BUILD_DIR= $BUILD_DIR"
 mkdir $BUILD_DIR
 
 cmake "$SCRIPT_DIR" -B"$BUILD_DIR" -DCMAKE_BUILD_TYPE=$1
-cmake  --build "$BUILD_DIR" --config $1
+cmake  --build "$BUILD_DIR" --config $1 -- -j4
